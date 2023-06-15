0c5df953bdbc0ff4089bc8caf26d1bd401252c01
Nathan B <33398449+nbendall34@users.noreply.github.com>
2023-06-15 21:18:40 +0100

    Update PaperCutMF-Minor-Release-Upgrade-V22.X.ps1
    Overhauled main functions, moved most of the varible definition to the top of the script

--- PaperCutMF-Minor-Release-Upgrade-V22.X.ps1
+++ PaperCutMF-Minor-Release-Upgrade-V22.X.ps1
@@ -1,110 +1,63 @@
-﻿# Get the path of the current script
+# Variables
 $scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Path
+$pcmfBackupLocation = "C:\EDUIT"
+$logFilePath = Join-Path -Path $scriptPath -ChildPath "papercut-upgrade.log"
+$pcmfCDNUrl = "https://cdn.papercut.com/web/products/ng-mf/installers/mf/22.x/pcmf-setup-22.1.1.66714.exe"
+$pcmfInstallerName = [regex]::Match($pcmfCDNUrl, '/([^/]+)$').Groups[1].Value
+$pcmfOldVersion = Get-ItemProperty -Path 'HKLM:\SOFTWARE\PaperCut MF' -Name 'Version' | Select-Object -ExpandProperty 'Version'
+$pcmfInstallPath = Get-ItemProperty -Path 'HKLM:\SOFTWARE\PaperCut MF' -Name 'InstallPath' | Select-Object -ExpandProperty 'InstallPath'
 
-# Check if the PaperCut installation folder exists in the C drive
-$pcInstallationLocationC = "C:\Program Files\PaperCut MF"
-if (Test-Path -Path $pcInstallationLocationC -PathType Container) {
-    $pcInstallationLocation = $pcInstallationLocationC
-}
-else {
-    # Check if the PaperCut installation folder exists in the E drive
-    $pcInstallationLocationE = "E:\Program Files\PaperCut MF"
-    if (Test-Path -Path $pcInstallationLocationE -PathType Container) {
-        $pcInstallationLocation = $pcInstallationLocationE
-    }
-    else {
-        Write-Host "Error: PaperCut installation folder not found."
-        return
-    }
-}
-
-# Define the source and destination paths
-$sourcePath = $pcInstallationLocation
-$destinationPath = "C:\EDUIT\PaperCut-Backup\22.1.1"
-
-# Define the log file path
-$logFilePath = Join-Path -Path $scriptPath -ChildPath "papercut-upgrade-log.dat"
-
-# Function to write log entries to the log file
+# Logging and Console Printing 
 function Write-LogEntry {
     param(
-        [string]$Message
+        [string]$Data
     )
-
     $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-    $logEntry = "[${timestamp}] $Message"
-
+    $logEntry = "[${timestamp}] $data"
+    Write-Output -InputObject "$($data)"
     $logEntry | Out-File -FilePath $logFilePath -Append
 }
 
-# Echo step: Stop PaperCut Server
-Write-Host "Stopping PaperCut Server..."
-Write-LogEntry -Message "Stopping PaperCut Server"
+# Stop PaperCut Server
+Write-LogEntry -Data "Stopping PaperCut Server"
 Stop-Service -Name "PaperCut Application Server"
 
-# Echo step: Create Backup Folder
-$backupFolder = Join-Path -Path $destinationPath -ChildPath "PaperCut MF"
+# Create Backup Folder
+$backupFolder = Join-Path -Path "$($pcmfBackupLocation)\PaperCut-Backup\$($pcmfOldVersion)" -ChildPath "PaperCut MF"
 if (!(Test-Path -Path $backupFolder -PathType Container)) {
-    Write-Host "Creating Backup Folder..."
-    Write-LogEntry -Message "Creating Backup Folder: $backupFolder"
+    Write-LogEntry -Data "Creating Backup Folder: $backupFolder"
     New-Item -Path $backupFolder -ItemType Directory
 } else {
-    Write-Host "Backup folder already exists. Skipping backup creation."
-    Write-LogEntry -Message "Backup folder already exists. Skipping backup creation: $backupFolder"
+    Write-LogEntry -Data "Backup folder already exists. Skipping backup creation: $backupFolder"
 }
 
 # Copy the installation folder to the backup folder
-Write-Host "Copying installation folder to backup..."
-Write-LogEntry -Message "Copying installation folder to backup: $sourcePath to $backupFolder"
-Copy-Item -Path $sourcePath -Destination $backupFolder -Recurse -Force
+Write-LogEntry -Data "Copying installation folder to backup: $pcmfInstallPath to $backupFolder"
+Copy-Item -Path $pcmfInstallPath -Destination $backupFolder -Recurse -Force
 
-# Echo step: Backup Complete
-Write-Host "Backup complete."
-Write-LogEntry -Message "Backup complete: $backupFolder"
-
-# Specify the URL of the exe file
-$downloadUrl = "https://cdn.papercut.com/web/products/ng-mf/installers/mf/22.x/pcmf-setup-22.1.1.66714.exe"
-
-# Specify the filename for the downloaded file
-$fileName = "pcmf-setup-22.1.1.66714.exe"
-
-# Specify the path to save the downloaded file
-$exePath = Join-Path -Path $scriptPath -ChildPath $fileName
+# Backup Complete
+Write-LogEntry -Data "Backup complete and saved to: $backupFolder"
 
 # Download the file
-Write-Host "Downloading the upgrade file..."
-Write-LogEntry -Message "Downloading the upgrade file"
-Invoke-WebRequest -Uri $downloadUrl -OutFile $exePath
+Write-LogEntry -Data "Downloading the upgrade file"
+Start-BitsTransfer -Source $pcmfCDNUrl -Destination (Join-Path -Path $scriptPath -ChildPath $pcmfInstallerName)
 
 # Check if the file was downloaded successfully
-if (Test-Path -Path $exePath -PathType Leaf) {
-    # Echo step: Run PaperCut Upgrade
-    Write-Host "Running PaperCut Upgrade..."
-    Write-LogEntry -Message "Running PaperCut Upgrade"
-    Start-Process -FilePath $exePath -ArgumentList "/VERYSILENT"
+if (Test-Path -Path (Join-Path -Path $scriptPath -ChildPath $pcmfInstallerName) -PathType Leaf) {
+    # Run PaperCut Upgrade
+    Write-LogEntry -Data "Running PaperCut Upgrade"
+    Start-Process -FilePath (Join-Path -Path $scriptPath -ChildPath $pcmfInstallerName) -ArgumentList "/VERYSILENT"
 } else {
-    Write-Host "Error: Failed to download the upgrade file."
-    Write-LogEntry -Message "Error: Failed to download the upgrade file."
+    Write-LogEntry -Data "Error: Failed to download the upgrade file."
 }
 
-# Echo step: Start PaperCut Server
-Write-Host "Starting PaperCut Server..."
-Write-LogEntry -Message "Starting PaperCut Server, please wait."
+# Start PaperCut Server
+Write-LogEntry -Data "Starting PaperCut Server, please wait."
 Start-Service -Name "PaperCut Application Server"
 
+# Get the current version number from registry
+$currentVersion = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\PaperCut MF' -Name 'Version' | Select-Object -ExpandProperty 'Version')
+Write-LogEntry -Data "Current version: $($currentVersion)"
 
-# Wait for 10 minutes
-# Start-Sleep -Seconds (10 * 60)
-
-# Get the current version number from version.txt
-$versionFilePath = Join-Path -Path $pcInstallationLocation -ChildPath "server\version.txt"
-if (Test-Path -Path $versionFilePath -PathType Leaf) {
-    $currentVersion = Get-Content -Path $versionFilePath -Raw
-    Write-LogEntry -Message "Current version: name-with-version=$currentVersion"
-} else {
-    Write-LogEntry -Message "Error: Failed to retrieve the current version number."
-}
-
-# Echo: Upgrade completed
-Write-Host "Upgrade completed."
-Write-LogEntry -Message "Upgrade completed."
+# Upgrade completed
+Write-LogEntry -Data "Upgrade completed."
